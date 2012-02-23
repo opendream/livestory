739cbc10c7aa6a9f7de0ac2241420b8623707e3d
crosalot <crosalot@gmail.com>
2012-02-23 15:15:31 +0700

    complete update profile without image

--- account/forms.py
+++ account/forms.py
@@ -5,6 +5,34 @@ from django.utils.translation import ugettext_lazy as _
 
 class AccountInviteForm(forms.Form):
     invite = forms.CharField(widget=forms.Textarea)
+    
+class AccountProfileForm(forms.Form):
+    firstname = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'input-xlarge'}), required=False)
+    lastname = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'input-xlarge'}), required=False)
+    
+    just_update_password = forms.IntegerField(required=False)
+    
+    password = forms.CharField(max_length=200, widget=forms.PasswordInput(attrs={'class': 'input-xlarge'}, render_value=False), required=False)
+    confirm_password = forms.CharField(max_length=200, widget=forms.PasswordInput(attrs={'class': 'input-xlarge'}, render_value=False), required=False)
+    
+    
+    
+    def clean_password(self):
+        if self.cleaned_data.get('just_update_password'):
+            raise forms.ValidationError(_("Password must be update"))
+            
+        return self.cleaned_data['password']
+        
+    def clean_confirm_password(self):
+        if self.cleaned_data.get('just_update_password'):
+            raise forms.ValidationError(_("Password must be confirm"))
+            
+        password         = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        
+        if password != confirm_password:
+            raise forms.ValidationError(_("Password not match"))
+        return confirm_password
 
 class EmailAuthenticationForm(forms.Form):
     """
