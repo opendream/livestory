1a6985e9456a4f73bb1dd73e93ae2d5aea20f6fc
Nirut Khemasakchai <rutcreate@gmail.com>
2012-02-24 17:28:22 +0700

    Blog post image preview

--- blog/views.py
+++ blog/views.py
@@ -42,88 +42,95 @@ def blog_create(request):
 
 @login_required
 def blog_edit(request, blog_id):
-    blog = Blog.objects.get(pk=blog_id)
-    location = Location.objects.get(pk=blog.location_id)
-    action = reverse('blog_edit', args=[blog_id])
-    if request.method == 'POST':
-        form = BlogEditForm(request.POST, request.FILES)
-        if form.is_valid():
-            blog.title = form.cleaned_data.get('title')
-            blog.description = form.cleaned_data.get('description')[:300]
-            blog.mood = form.cleaned_data.get('mood')
-            blog.category = form.cleaned_data.get('category')
-            blog.location = blog_save_location(form.cleaned_data.get('country'), form.cleaned_data.get('city'))
-            blog.private = form.cleaned_data.get('private')
+    try:
+        blog = Blog.objects.get(pk=blog_id)
+        location = Location.objects.get(pk=blog.location_id)
+        action = reverse('blog_edit', args=[blog_id])
+        if request.method == 'POST':
+            form = BlogEditForm(request.POST, request.FILES)
+            if form.is_valid():
+                blog.title = form.cleaned_data.get('title')
+                blog.description = form.cleaned_data.get('description')[:300]
+                blog.mood = form.cleaned_data.get('mood')
+                blog.category = form.cleaned_data.get('category')
+                blog.location = blog_save_location(form.cleaned_data.get('country'), form.cleaned_data.get('city'))
+                blog.private = form.cleaned_data.get('private')
 
-            # If previous is draft, you can draft it again.
-            if blog.draft:
-                blog.draft = bool(int(form.data.get('draft')))
-            
-            # There is image uploaded.
-            if request.FILES.get('image'):
-                image = request.FILES.get('image')
-                # Remove old image.
-                if os.path.isfile(blog.image.path):
-                    os.remove(blog.image.path)
-                # Save new image.
-                blog.image.save(image.name, image, save=False)
-            
-            blog.save()
-            return redirect('/blog/%s/edit' % blog.pk)
-    else:
-        defaults = {
-            'title': blog.title,
-            'description': blog.description,
-            'country': location.country,
-            'city': location.city,
-            'mood': str(blog.mood),
-            'category': blog.category,
-            'private': str(int(blog.private))
+                # If previous is draft, you can draft it again.
+                if blog.draft:
+                    blog.draft = bool(int(form.data.get('draft')))
+                
+                # There is image uploaded.
+                if request.FILES.get('image'):
+                    image = request.FILES.get('image')
+                    # Remove old image.
+                    if os.path.isfile(blog.image.path):
+                        os.remove(blog.image.path)
+                    # Save new image.
+                    blog.image.save(image.name, image, save=False)
+                
+                blog.save()
+                return redirect('/blog/%s/edit' % blog.pk)
+        else:
+            defaults = {
+                'title': blog.title,
+                'description': blog.description,
+                'country': location.country,
+                'city': location.city,
+                'mood': str(blog.mood),
+                'category': blog.category,
+                'private': str(int(blog.private))
+            }
+            form = BlogEditForm(defaults)
+
+        context = {
+            'form': form,
+            'moods': MOOD_CHOICES,
+            'visibilities': PRIVATE_CHOICES,
+            'image_path': blog.get_image_url(),
+            'is_draft': blog.draft,
+            'blog': blog
         }
-        form = BlogEditForm(defaults)
-
-    context = {
-        'form': form,
-        'moods': MOOD_CHOICES,
-        'visibilities': PRIVATE_CHOICES,
-        'image_path': blog.get_image_url(),
-        'is_draft': blog.draft,
-        'blog': blog
-    }
-        
-    return render(request, 'blog/blog_form.html', context)
+        return render(request, 'blog/blog_form.html', context)
+    except Blog.DoesNotExist:
+        pass
 
 def blog_view(request, blog_id):
-    blog = Blog.objects.get(pk=blog_id)
-    if blog.private:
-        # TODO redirect to access denied
-        pass
-    love_path = '/blog/%s/love/'
-    button_type = 'love'
     try:
-        # If this blog is already loved by current logged in user
-        love = Love.objects.get(user=request.user, blog=blog)
-        love_path = '/blog/%s/unlove/'
-        button_type = 'unlove'
-    except Love.DoesNotExist:
-        # Do nothing. Use default love_path and button_type.
-        pass
-    
-    love_set = blog.love_set.all().order_by('-datetime')
-    loved_users = []
-    for l in love_set:
-        loved_users.append(l.user.get_profile())
+        blog = Blog.objects.get(pk=blog_id)
+        if blog.private:
+            # TODO redirect to access denied
+            pass
+        love_path = '/blog/%s/love/'
+        button_type = 'love'
+        try:
+            # If this blog is already loved by current logged in user
+            love = Love.objects.get(user=request.user, blog=blog)
+            love_path = '/blog/%s/unlove/'
+            button_type = 'unlove'
+        except Love.DoesNotExist:
+            # Do nothing. Use default love_path and button_type.
+            pass
+        
+        love_set = blog.love_set.all().order_by('-datetime')
+        loved_users = []
+        for l in love_set:
+            loved_users.append(l.user.get_profile())
 
-    context = {
-        'blog': blog,
-        'profile': request.user.get_profile(),
-        'love_path': love_path % blog_id,
-        'button_type': button_type,
-        'love_count': Love.objects.filter(blog=blog).count(),
-        'loved_users': loved_users,
-        'max_items': 7
-    }
-    return render(request, 'blog/blog_view.html', context)
+        context = {
+            'blog': blog,
+            'profile': request.user.get_profile(),
+            'love_path': love_path % blog_id,
+            'button_type': button_type,
+            'love_count': Love.objects.filter(blog=blog).count(),
+            'loved_users': loved_users,
+            'max_items': 7
+        }
+        return render(request, 'blog/blog_view.html', context)
+    except Blog.DoesNotExist:
+        # Page not found
+        # TODO
+        return HttpResponse('blog post not found')
 
 @login_required
 def blog_love(request, blog_id):
