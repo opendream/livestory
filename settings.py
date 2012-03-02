822a0d9dee800a39442b1c4d5ee0e4150e21918b
crosalot <crosalot@gmail.com>
2012-03-02 19:40:38 +0700

    new fixture

--- settings.py
+++ settings.py
@@ -158,13 +158,6 @@ INSTALLED_APPS = (
     'location',
 )
 
-FIXTURE_DIRS = (
-    'auth/fixtures'
-    'account/fixtures/',
-    'blog/fixtures/',
-    'location/fixtures/',
-)
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
