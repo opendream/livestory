2cdafe7368ef0d56e57b309f182ab65a8fad7a02
juacompe <juacompe@gmail.com>
2012-03-04 12:52:12 +0700

    - add test_manage_blog__GET__parameters_passed_in_blog_manager

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from blog.models import Blog
-from mock import Mock
+from mock import Mock, patch
 from tests import factory
 
 class MyMock(object):
@@ -9,35 +9,63 @@ class MyMock(object):
         for key in kwargs.keys():
             setattr(self, key, kwargs[key])
 
-class TestBlogManagement(TestCase):
+class BaseTestBlogManagement(TestCase):
     def setUp(self):
+        sprite = MyMock(title = 'Sprite')
+        coke = MyMock(title = 'Coke')
+        pepsi = MyMock(title = 'Pepsi')
+        self.blogs = [sprite, coke, pepsi]
+    
+class TestBlogManagementUsingPatch(BaseTestBlogManagement):
+    # Test only view function, model layer is mocked
+    @patch('blog.models.Blog.objects.all')
+    def test_simple_get(self, mocked_all):
+        # Act
+        mocked_all.return_value = self.blogs
+        response = self.client.get('/blog/manage/')
+        # Assert
+        self.assertContains(response, 'Sprite')
+        self.assertContains(response, 'Coke')
+        self.assertContains(response, 'Pepsi')
+ 
+
+class TestBlogManagementUsingMock(BaseTestBlogManagement):
+    # Test only view function, model layer is mocked
+    def setUp(self):
+        super(TestBlogManagementUsingMock, self).setUp()
         self.mock_blog_manager()
 
     def tearDown(self):
         self.unmock_blog_manager()
+        super(TestBlogManagementUsingMock, self).tearDown()
     
     def mock_blog_manager(self):
-        sprite = MyMock(title = 'Sprite')
-        coke = MyMock(title = 'Coke')
-        pepsi = MyMock(title = 'Pepsi')
-        blogs = [sprite, coke, pepsi]
-
         self.original_manager = Blog.objects
         Blog.objects = self.mock_manager = Mock()
-        Blog.objects.all = Mock(return_value = blogs)
+        Blog.objects.all = Mock(return_value = self.blogs)
 
     def unmock_blog_manager(self):
         Blog.objects = self.original_manager
 
-    # Test only view function, model layer is mocked
     def test_simple_get(self):
         # Act
         response = self.client.get('/blog/manage/')
         # Assert
-        print 'content = ', response.content
         self.assertContains(response, 'Sprite')
         self.assertContains(response, 'Coke')
         self.assertContains(response, 'Pepsi')
+
+    def test_manage_blog__GET__parameters_passed_in_blog_manager(self):
+        """
+        Scenario: GET method to /blog/manage/
+        Expected:
+        - Blog.objects.all is called with no parameters
+        """
+        # Act
+        response = self.client.get('/blog/manage/')
+        # Assert
+        Blog.objects.all.assert_called_once_with()
+
     
 class TestBlogManagementWithModel(TestCase):
     def setUp(self):
