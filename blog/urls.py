7308c6f5e34557d2191c7468b0876b91e95b6e43
Nirut Khemasakchai <rutcreate@gmail.com>
2012-03-04 17:17:13 +0700

    change url /blog/manage/set/* to use /blog/manage/bulk/

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('blog.views',
     url(r'create/$', 'blog_create', name='blog_create'),
     url(r'manage/$', 'blog_manage', name='blog_manage'),
+    url(r'manage/bulk/$', 'blog_manage_bulk', name='blog_manage_bulk'),
     url(r'manage/set/private/$', 'blog_manage_set_private', name='blog_manage_set_private'),
     url(r'manage/set/public/$', 'blog_manage_set_public', name='blog_manage_set_public'),
     url(r'(?P<blog_id>\d+)/$', 'blog_view', name='blog_view'),
