276ce8af3b18809bb8af96a126c84143462ca96b
juacompe <juacompe@gmail.com>
2012-03-04 11:03:21 +0700

    - refactor Blog's unicode test - all test still pass

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -6,16 +6,23 @@ class TestBlog(TestCase):
     def setUp(self):
         self.category = factory.create_category()
         self.user = factory.create_user()
-
         self.location = factory.create_location() 
 
     def test_unicode(self):
+        """
+        Scenario: Given a blog named Icecream
+        Expected:
+        - its unicode should be (<id>) Icecream
+        """
+        # Arrage
         blog = Blog(title='Icecream') 
         blog.user = self.user
         blog.category = self.category
         blog.location = self.location
         blog.save()
         printed_unicode = '(%s) Icecream' % blog.id
-        self.assertEqual(printed_unicode, blog.__unicode__())
-
+        # Act
+        result = blog.__unicode__()
+        # Assert
+        self.assertEqual(printed_unicode, result)
 
