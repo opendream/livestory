5c9d969d652e62e4607b137f0fb32f40cb2f8148
crosalot <crosalot@gmail.com>
2012-03-05 17:44:26 +0700

    - add setUp to arrange data

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -3,9 +3,15 @@ from django.test import TestCase
 from tests import factory
 
 class TestAccount(TestCase):
+    def setUp(self):
+        self.users = [
+            factory.create_user('testlove1', 'tester1@example.com', 'testuser1', 'Nirut', 'Khemasakchai'),
+            factory.create_user('testlove2', 'tester2@example.com', 'testuser2', 'Panudate', 'Vasinwattana')
+        ]
+        
     def test_get_fullname(self):
-        user = factory.create_user('rutcreate1@gmail.com', 'rutcreate1@gmail.com', 'rutcreate', 'Nirut', 'Khemasakchai')
-        self.assertEquals('Nirut Khemasakchai', user.get_profile().get_fullname())
+        self.assertEquals('Nirut Khemasakchai', self.users[0].get_profile().get_fullname())
+        self.assertEquals('Panudate Vasinwattana', self.users[1].get_profile().get_fullname())
         
     def test_get_image(self):
         self.assertEquals('', '')
