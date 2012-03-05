0a03765a8767d4574df5e17592886ee78b21e79a
crosalot <crosalot@gmail.com>
2012-03-05 15:17:53 +0700

    change function test_get_fullname

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from tests import factory
 
 class TestAccount(TestCase):
-    def test_get_full_name(self):
+    def test_get_fullname(self):
         user = factory.create_user('rutcreate1@gmail.com', 'rutcreate1@gmail.com', 'rutcreate')
         account = Account(firstname='Nirut', lastname='Khemasakchai', user=user)
         account.save()
