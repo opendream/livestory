2945d531f720217f28b0ab7439014cb40b0c7514
crosalot <crosalot@gmail.com>
2012-03-05 14:28:27 +0700

    - remove old test file - add new test file - add test_get_fullname

---
+++ account/tests/test_models.py
@@ -0,0 +1,10 @@
+from account.models import Account
+from django.test import TestCase
+from tests import factory
+
+class TestAccount(TestCase):
+    def test_get_full_name(self):
+        user = factory.create_user('rutcreate1@gmail.com', 'rutcreate1@gmail.com', 'rutcreate')
+        account = Account(firstname='Nirut', lastname='Khemasakchai', user=user)
+        account.save()
+        self.assertEquals('Nirut Khemasakchai', user.get_profile().get_fullname())
