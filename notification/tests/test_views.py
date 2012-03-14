94c5f98e4ea116b7ef83f65efa6c34542995499b
Nirut Khemasakchai <rutcreate@gmail.com>
2012-03-14 14:45:37 +0700

    - refactor: change test name. tested

--- notification/tests/test_views.py
+++ notification/tests/test_views.py
@@ -28,7 +28,7 @@ class TestHelperFunction(TestCase):
 		self.assertEqual(0, len(get_notifications(self.user)))
 		self.assertEqual(1, len(get_notifications(self.user_with_notification)))
 
-	def test_get_notifications_after_view(self):
+	def test_get_notifications_after_viewed(self):
 		self.assertEqual(1, len(get_notifications(self.user_with_notification)))
 		AccountKey.objects.get(user=self.user_with_notification).update_view_notification()
 		self.assertEqual(0, len(get_notifications(self.user_with_notification)))
