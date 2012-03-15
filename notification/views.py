5041c202eda23828b4b13b3a9dce5d270c6b62b6
Nirut Khemasakchai <rutcreate@gmail.com>
2012-03-15 09:25:23 +0700

    add logic for display notification last 7 days

--- notification/views.py
+++ notification/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect, render
 from django.utils import simplejson as json
 from notification.models import Notification
 
+from datetime import datetime, timedelta
+
 def view(request):
 	if not request.user.is_authenticated():
 		return render(request, '403.html', status=403)
@@ -16,7 +18,16 @@ def view(request):
 		print json.dumps({'status': 200})
 		return HttpResponse(json.dumps({'status': 200}), mimetype='application/json')
 
-	context = {}
+	last_notification = Notification.objects.all().order_by('-datetime')[:1]
+	last_seven_days = last_notification[0].datetime - timedelta(7)
+	print 'last 7 days = ', last_seven_days
+	notifications = Notification.objects.filter(datetime__gt=last_seven_days)
+	for notification in notifications:
+		print notification.subject.get_profile().get_fullname(), notification.datetime
+
+	context = {
+		'notification7days': notifications
+	}
 	return render(request, 'notification/notification_view.html', context)
 
 def get_notifications(user):
