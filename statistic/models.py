6d73c3f89c55329cabc70530b31b9fd899a01783
Nirut Khemasakchai <rutcreate@gmail.com>
2012-03-21 09:44:38 +0700

    - add model ViewCount - add class TestBaseData for prepare data - add test create ViewCount

--- statistic/models.py
+++ statistic/models.py
@@ -3,7 +3,16 @@ from django.db import models
 
 from blog.models import Blog
 
+
 class History(models.Model):
 	datetime = models.DateTimeField(auto_now_add=True)
 	user = models.ForeignKey(User)
 	blog = models.ForeignKey(Blog)
+
+
+class ViewCount(models.Model):
+	datetime = models.DateTimeField(auto_now_add=True)
+	blog = models.ForeignKey(Blog)
+	totalcount = models.IntegerField(default=1)
+	weekcount = models.IntegerField(default=1)
+	daycount = models.IntegerField(default=1)
--- statistic/tests/test_models.py
+++ statistic/tests/test_models.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 
 from common import rm_user
-from statistic.models import History
+from statistic.models import History, ViewCount
 from tests import factory
 
-class TestHistory(TestCase):
+class TestBaseData(TestCase):
 	def setUp(self):
 		self.user = factory.create_user('john.doe@example.com', 'john.doe@example.com', '1234', 'John', 'Doe')
 		self.category = factory.create_category('Travel', 'travel')
@@ -17,6 +17,16 @@ class TestHistory(TestCase):
 		self.location.delete()
 		self.blog.delete()
 
+class TestHistory(TestBaseData):
 	def test_create(self):
 		History.objects.create(user=self.user, blog=self.blog)
 		self.assertEqual(1, History.objects.filter(blog=self.blog).count())
+
+
+class TestViewCount(TestBaseData):
+	def test_create(self):
+		view = ViewCount.objects.create(blog=self.blog, totalcount=3, weekcount=2, daycount=1)
+		self.assertEqual(view.blog.id, self.blog.id)
+		self.assertEqual(view.totalcount, 3)
+		self.assertEqual(view.weekcount, 2)
+		self.assertEqual(view.daycount, 1)
