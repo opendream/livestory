7e869e0feed75d0f47bd42c1c11ce26d727ca926
Tavee Khunbida <taveek@gmail.com>
2012-06-08 15:44:20 +0700

    Improve notification email content.

--- notification/management/commands/loves_notifier.py
+++ notification/management/commands/loves_notifier.py
@@ -10,28 +10,35 @@ import os
 class Command(NoArgsCommand):
     
     def __init__(self, *args, **kwargs):
-        self.TXT_TEMPLATE = 'notification/email/loved_notification_email.txt'
+        self.TXT_TEMPLATE  = 'notification/email/loved_notification_email.txt'
         self.HTML_TEMPLATE = 'notification/email/loved_notification_email.html'
-        self.LOG_FILE = os.path.join(settings.BASE_PATH, "log/loves_notifier.log")
+        self.LOG_FILE      = os.path.join(settings.BASE_PATH, "log/loves_notifier.log")
         super(Command, self).__init__(*args, **kwargs)
 
     def handle(self, *args, **options):
-        logger = open(self.LOG_FILE, 'a+')
-        now = datetime.now()
-        loved_items = self.collect_love_nofications(now - timedelta(days=1))
-        receivers = set([item.blog.user for item in loved_items])
+        logger      = open(self.LOG_FILE, 'a+')
+        target_date = datetime.now() - timedelta(days=1)
+        loved_items = self.collect_love_nofications(target_date)
+        receivers   = set([item.blog.user for item in loved_items])
         
         if len(receivers) > 0:
             notifications = []
             base_url = self.get_base_url()
 
             for receiver in receivers:
-                receiver_items = loved_items.filter(blog__user=receiver)
-                notifications.append(self.prepare_notification_emails(base_url, receiver, receiver_items))
+                email = self.prepare_notification_emails(
+                    base_url, 
+                    receiver, 
+                    loved_items.filter(
+                        blog__user=receiver
+                    ),
+                    target_date
+                )
+                notifications.append(email)
 
             async.map(notifications)
 
-        logger.write("[%s] Sending %d notification emails.\n" % (now, len(receivers)))
+        logger.write("[%s] Sending %d notification emails.\n" % (datetime.now(), len(receivers)))
         logger.close()
 
     def collect_love_nofications(self, for_date):
@@ -46,12 +53,14 @@ class Command(NoArgsCommand):
         site = Site.objects.get(id=settings.SITE_ID)
         return site.domain if site else None
 
-    def prepare_notification_emails(self, base_url, user, loved_items):
+    def prepare_notification_emails(self, base_url, user, loved_items, target_date):
+        activaty_date_str = target_date.strftime("%A, %b %Y")
         email_context = {
             'loved_items': loved_items,
             'base_url'   : base_url,
             'settings'   : settings, 
-            'user'       : user
+            'user'       : user,
+            'date'       : activaty_date_str,
         }
 
         return async.post('%s/messages' % settings.MAILGUN_API_DOMAIN,
@@ -59,7 +68,7 @@ class Command(NoArgsCommand):
             data={
                 'from'   : settings.DEFAULT_FROM_EMAIL,
                 'to'     : user.email,
-                'subject': 'Someone loves your blog.',
+                'subject': 'Your Oxfam Live Stories activity for %s' % activaty_date_str,
                 'text'   : render_to_string(self.TXT_TEMPLATE, email_context),
                 'html'   : render_to_string(self.HTML_TEMPLATE, email_context),
             }
